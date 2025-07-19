d9d29dd050f042fb6b6f2c17915c5a0ebe0bf8e8
Qasimjamil7 <qasimjamil2107@gmail.com>
2025-07-19 21:26:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// You can add interactive features here
+console.log("Portfolio script loaded.");
+
+// Example: scroll effect or animation setup
+window.addEventListener("scroll", () => {
+  // add animation logic later
+});
